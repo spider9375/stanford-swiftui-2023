321f86334f3b826cf1d4d595f5c1ca1213aa0b5d
Dimitar Mihaylov <dimitar.mihaylove@gmail.com>
2024-09-30 19:29:57 +0300

    Initial Commit CardView isFaceUp

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Dimitar Mihaylov on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
